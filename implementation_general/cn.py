782be3309eada6c1feefdb0fa3638ffc77e22929
SANTAMARIA GARCIA Simon <s1475174@ed.ac.uk>
2020-06-15 17:13:28 +0100

    Estructure of the code is way under way. Creates massive data structure that stores temperature data at all conditions studied. Still need to arrange for modifications in A matrix and b vector according to the boundary conditions

--- implementation_general/cn.py
+++ implementation_general/cn.py
@@ -1,108 +1,162 @@
 """
-Crank-Nicholson implementation of 1D heat transfer with simplified boundary conditions for comparison with
-analytical solutions.
+Crank-Nicholson implementation of 1D heat transfer
 """
 
 # import libraries
 import numpy as np
 
 
-def cn_solver(x_grid, t_grid, upsilon, bc, bc_data, space_divisions, T_initial, dx, k):
+def general_temperatures(hf_type, T_initial, T_air, time_total, k, alpha, dx, x_grid, space_divisions, dt_all,
+                    t_grid, upsilon, bc_surface, bc_back):
     """
-    Applies Crank-Nicolson method to numerically solve the heat diffusion over the specified domain
+    General function to implement CN.
+    Creates different functions of time for the incident heat flux.
+    Returns dictionary that includes all calculated temperature for given boundary conditions evaluated at different
+    properties and parameters
     
     Validation function: Exposed surface uses dirichlet, neunman or robin boundary conditions.
     Semi-infinite solid
     
+    CN Scheme:
+    ---------
+        [A]{T^n+1} = [B]{T^n} = [b]
+    
+    
     Parameters:
     ----------
-    x_grid: one dimensional spatial domain
+    hf_type: type of heat flux function for the incident heat flux.
+        str
+    
+    T_initial: initial temperature in K
+        int
+        
+    T_air: air(infinity) temperature in K for convective losses. Usually equals T_initial but not necessary
+        int        
+
+    time_total: total time for calculations
+        int
+        
+    k: thermal conductivity in W/mK. Array with range of values
         np.array
         
-    t_grid: temporal domain
+    alpha: thermal diffusivity in m2/s. Array with range of values
         np.array
+        
+    dx: size of cell in space domain in m
+        float 
+    
+    x_grid: one dimensional spatial domain
+        np.array
+
+    space_divisions: number of nodes in the spatial domain
+        int
+
+    dt_all: different thermal diffusivities define different time steps for a given dx
+        np.array
+
+    t_grid: list where each entry is the temporal domain for a given alpha and dt
+        list
         
     upsilon: Fourier number divided by 2
         float
         
-    bc: boundary condition
+    bc_surface: surface boundary condition (linear or non-linear)
         str
         
-    bc_data: constant surface temperature (Dirichlet), surface heat flux (neunman), h and T_gas (Robin)
-        int or tuple    
-        
-    space_divisions: number of nodes in the spatial domain
-        int
-        
-    T_initial: initial temperature in K
-        int
-
-    dx = size of cell in space domain in m
-        float
-        
-    k = thermal conductivity in W/mK
-        float
+    bc_back: back boundary condition (insulation or conductive losses to aluminium block)
+        str
 
     Returns:
     -------
-    Temperature: dictionary with array of temperatures for each time
-        np.array
+    Temperature: dictionary with temperature arrays for different alpha and k
+        dict
     
     """
-    
-    # create dictionary to store all temperatures at all times
-    Temperature = {}
-    
-    # create matrix A
-    A = tridiag_matrix(bc, upsilon, space_divisions, bc_data, dx, k)
-    
-    # initialise temperature arrays for present and future temperatures
-    T = np.zeros_like(x_grid) + T_initial;
-    Tn = np.zeros_like(x_grid)
-    
-    for dt in t_grid:
-        
-        # create vector b
-        b = vector_b(bc, space_divisions, upsilon, T, bc_data, T_initial, dx, k)
-        
-        # calculate value of future temperature
-        Tn = np.linalg.solve(A,b)
-        
-        # append temperature values to dictionary
-        Temperature[dt] = Tn
-        
-        # update present temperature
-        T = Tn.copy()
-    
-    return Temperature
 
+    # according to the type of heat flux, different parameters are used to define the function
+    if hf_type == "Constant":
+        q = np.linspace(20,80,4)
+    elif hf_type == "Linear":
+        q = np.linspace(100,700,4)
+    elif hf_type == "Quadratic":
+        q = np.linspace(2.5e-5, 1e-4,4)
+
+    temperatures = {}
+
+    # iterate over the different heat flux defined above
+    for heat_flux in q:
+    
+        temperatures[f"q:_{heat_flux}"] = {}
+        
+        # iterate over all value pairs of alpha and k
+        for i in range(len(alpha)):
+            k_this = k[i]
+            upsilon_this = upsilon[i]
+            alpha_this = alpha[i]
+            t_grid_this = t_grid[i]
+            
+            # create tridiagonal matrix A
+            A = tridiag_matrix(bc_surface, bc_back, upsilon_this, space_divisions, dx, k_this)
+            
+            # initialise temperature arrays for present and future temperatures
+            T = np.zeros_like(x_grid) + T_initial
+            Tn = np.zeros_like(x_grid)
+            
+            # define the incident heat flux
+            if hf_type == "Constant":
+                q_array = np.zeros_like(t_grid_this) + heat_flux
+            elif hf_type == "Linear":
+                q_array = heat_flux * t_grid_this
+            elif hf_type == "Quadratic":
+                q_array = heat_flux * t_grid_this * t_grid_this
+            
+            temperatures[f"q:_{heat_flux}"][f"alpha_{alpha_this}"] = {}
+            
+            # iterate over each time step
+            for t in t_grid_this:
+                
+                # create vector b
+                b = vector_b(bc_surface, bc_back, upsilon_this, space_divisions, dx, k, T, T_initial, q_array)
+                
+                # calculate value of future temperature
+                Tn = np.linalg.solve(A,b)
+            
+                # update present temperature
+                T = Tn.copy()
+
+                # store temperature profile at this time in the overall dictionary
+                temperatures[f"q:_{heat_flux}"][f"alpha_{alpha_this}"][f"t_{t}"] = Tn
+            
+    return temperatures   
+    
+    
 
 # function to create tri-diagonal matrix
-def tridiag_matrix(bc, upsilon, space_divisions, bc_data, dx, k):
+def tridiag_matrix(bc_surface, bc_back, upsilon, space_divisions, dx, k):
     """
     Creates tridiagonal matrix A
     Linear system to be solved is Ax = b, and x represents temperature values at time n+1
 
     Parameters:
     ----------
-    bc: boundary condition
+    bc_surface: boundary condition at the surface
         str
     
-    upsilon: Fourier number divided by 2 = alpha*dt/2*dx2
+    bc_back: boundary condition at the back
+        str
+    
+    upsilon: Fourier number divided by 2. Upsilon = alpha*dt/2*dx2
         float
         
     space_divisions: number of nodes in the spatial domain
         int
-
-    bc_data: constant surface temperature (Dirichlet), surface heat flux (neunman), h and T_gas (Robin)
-        int or tuple 
         
-    dx = size of cell in space domain in m
+    dx: size of cell in space domain in m
         float
         
-    k = thermal conductivity in W/mK
+    k: thermal conductivity in W/mK
         float
-            
+    
     Return:
     ------
     
@@ -116,56 +170,67 @@ def tridiag_matrix(bc, upsilon, space_divisions, bc_data, dx, k):
         np.diagflat([-upsilon for i in range(space_divisions - 1)], 1)
 
     # adjust matrix depending on the boundary condition at the exposed surface
-    if bc == "Dirichlet":
-        A[0, 0] = 1
-        A[0, 1] = 0
-    elif bc == "Neunman":
-        A[0,0] = 1 + 2*upsilon
-        A[0,1] = - 2 * upsilon
-    elif bc == "Robin":
+    if bc_surface == "Linear":
         
-        h, _ = bc_data
-        A[0,0] = 1 + 2*upsilon + 2*dx*h/k
-        A[0,1] = - 2 * upsilon
-        
-    # adjust matrix for the boundary condition at the unexposed surface
-    A[-1, -2] = - 2 * upsilon
-    A[-1, -1] = 1 + 2 * upsilon
+        h_total = 45
+        pass
+    
+    elif bc_surface == "Non-linear":
+        pass
+    
 
+#    elif bc == "Robin":
+#        
+#        h, _ = bc_data
+#        A[0,0] = 1 + 2*upsilon + 2*dx*h/k
+#        A[0,1] = - 2 * upsilon
+    
+    # adjust matrix for the back boundary conditions
+    if bc_back == "Insulation":
+        pass
+    
+    elif bc_back == "Aluminium_block":
+        pass
+#    A[-1, -2] = - 2 * upsilon
+#    A[-1, -1] = 1 + 2 * upsilon
+#
     return A
 
 
 
-#def vector_b(upsilon, space_divisions, T, BC_tuple, BC_values, dx, material, i, solving_method):
-def vector_b(bc, space_divisions, upsilon, T, bc_data, T_initial, dx, k):
+
+def vector_b(bc_surface, bc_back, upsilon, space_divisions, dx, k, T, T_initial, q_array):
     """
     Calculates vector b. Right hand side of linear system of equations
 
     Parameters:
     ----------
-    bc: boundary condition
+    bc_surface: boundary condition at the surface
         str
-
+    
+    bc_back: boundary condition at the back
+        str
+    
+    upsilon: Fourier number divided by 2. Upsilon = alpha*dt/2*dx2
+        float
+        
     space_divisions: number of nodes in the spatial domain
         int
         
-    upsilon: Fourier number divided by 2 = alpha*dt/2*dx2
+    dx: size of cell in space domain in m
+        float
+        
+    k: thermal conductivity in W/mK
         float
         
     T: array of present temperatures
         np.array
-        
-    bc_data: constant surface temperature (Dirichlet), surface heat flux (neunman), h and T_gas (Robin)
-        int or tuple
-        
+
     T_initial: initial temperature in K
         int
         
-    dx = size of cell in space domain in m
-        float
-        
-    k = thermal conductivity in W/mK
-        float
+    q_array: array of size t_grid that contains the incident heat flux at each time step
+        np.array
     
     Returns:
     -------
@@ -174,33 +239,33 @@ def vector_b(bc, space_divisions, upsilon, T, bc_data, T_initial, dx, k):
     """
     
     # matrix B, similar to matrix A but multiplies T at present
-    B = np.diagflat([upsilon for i in range(space_divisions - 1)], -1) +\
-        np.diagflat([1 - 2 * upsilon for i in range(space_divisions)]) +\
-        np.diagflat([upsilon for i in range(space_divisions - 1)], 1)
+#    B = np.diagflat([upsilon for i in range(space_divisions - 1)], -1) +\
+#        np.diagflat([1 - 2 * upsilon for i in range(space_divisions)]) +\
+#        np.diagflat([upsilon for i in range(space_divisions - 1)], 1)
 
     # Calculate vector b
     b = np.zeros(space_divisions)
-    b[1:-1] = B[1:-1, :].dot(T)
-
-    # adjust b vector depending on the surface
-    if bc == "Dirichlet":
-        
-        T_surface = bc_data
-        b[0] = T_surface
-        
-    elif bc == "Neunman":
-        
-        q = bc_data
-        b[0] = 2*upsilon*T[1] + (1 - 2*upsilon)*T[0] + (4*dx*q*upsilon)/(k)
-        
-    elif bc == "Robin":
-        
-        h, T_gas = bc_data
-        b[0] =  2*upsilon*T[1] + (1 - 2*upsilon - 2*dx*h/k)*T[0] + 4*dx*h*T_gas/k
-    
-    # adjust b vector to the back boundary conditions (semi-infinite)
-    b[-2] = T_initial
-    b[-1] = T_initial
+#    b[1:-1] = B[1:-1, :].dot(T)
+#
+#    # adjust b vector depending on the surface
+#    if bc == "Dirichlet":
+#        
+#        T_surface = bc_data
+#        b[0] = T_surface
+#        
+#    elif bc == "Neunman":
+#        
+#        q = bc_data
+#        b[0] = 2*upsilon*T[1] + (1 - 2*upsilon)*T[0] + (4*dx*q*upsilon)/(k)
+#        
+#    elif bc == "Robin":
+#        
+#        h, T_gas = bc_data
+#        b[0] =  2*upsilon*T[1] + (1 - 2*upsilon - 2*dx*h/k)*T[0] + 4*dx*h*T_gas/k
+#    
+#    # adjust b vector to the back boundary conditions (semi-infinite)
+#    b[-2] = T_initial
+#    b[-1] = T_initial
     
     return b    
     
--- implementation_general/main.py
+++ implementation_general/main.py
@@ -8,7 +8,7 @@ a) q_inc - q _conv - q_rad (where q_inc is a function of time)
 b) q_inc - q_losses (using a total heat transfer coefficient)
 Unexposed: 
 a) conductive losses to an aluminium block
-b) semi-infinite solid assumption
+b) insulated boundary
 
 
 1-D. Inet solid. Homogoneous. Constant properties.
@@ -21,11 +21,14 @@ import pickle
 import time
 
 # import my own function
-from cn import cn_solver
+from cn import general_temperatures
+
+# analyse three types of incident heat fluxes
+heat_fluxes = ["Constant", "Linear", "Quadratic"]
 
 # create list with boundary conditions to be validated
 boundary_conditions_surface = ["Linear", "Non-linear"]
-boundary_conditions_back = ["Semi-infinite", "Aluminium_block"]
+boundary_conditions_back = ["Insulation", "Aluminium_block"]
 
 # create a dictionary to store temperature profiles for all boundary conditions and all times
 Temperatures = {}
@@ -33,7 +36,7 @@ Temperatures = {}
 # parameters for the calculations
 T_initial = 288                      # K
 T_air = T_initial                    # K
-time_total = 901                     # s
+time_total = 51                     # s
 sample_length = 0.025                # m
 space_divisions = 100                # -
 
@@ -53,145 +56,40 @@ for i,dt in enumerate(dt_all):
     t_grid.append(np.array([n * dt for n in range(int(time_divisions[i]))]))
     
 upsilon = (alpha*dt)/(2*dx**2)
+
+# iterate over three possible types of heat fluxes
+for hf_type in heat_fluxes:
     
-# iterate over surface boundary conditions
-for bc_surface in boundary_conditions_surface:
+    print("---------")
+    print(f" {hf_type} heat flux ")
+    print("---------")
+    Temperatures[f"hf-type:_{hf_type}"] = {}
+
+    # iterate over surface boundary conditions
+    for bc_surface in boundary_conditions_surface:
+        
+        print(f"-- Surface boundary condition {bc_surface} --")
+        Temperatures[f"hf-type:_{hf_type}"][f"Surface_{bc_surface}"] = {}
+        
+        # iterate over back boundary conditions
+        for bc_back in boundary_conditions_back:
+            
+            print(f" -Back boundary condition {bc_back}-")
+            
+            start = time.time()
+            Temperatures[f"hf-type:_{hf_type}"][f"Surface_{bc_surface}"][f"Back_{bc_back}"] = general_temperatures(hf_type, T_initial, T_air, time_total, k, alpha, dx, x_grid,
+                        space_divisions,dt_all,t_grid, upsilon, bc_surface, bc_back)
+            
+            print(f"Time taken for {bc_surface} and {bc_back}: {np.round(time.time() - start,2)} seconds")
+
+
+# condense all data to be saved including important plotting parameters
+total_data = {"Temperatures": Temperatures, "extra_data": {
+        "t_grid":t_grid, "x_grid":x_grid, "time_total": time_total},
+    }
+
+# save in a pickle to retrieve and plot later
+with open('total_data_general.pickle', 'wb') as handle:
+    pickle.dump(total_data, handle)
+    print("All data saved into total_data_general.pickle")
     
-    print(f"-- Surface boundary condition {bc_surface} --")
-    Temperatures[bc_surface] = {}
-    
-    # iterate over back boundary conditions
-    for bc_back in boundary_conditions_back:
-        
-        print(f" -Back boundary condition {bc_back}-")
-        Temperatures[bc_surface][bc_back] = {}
-        
-        
-
-
-
-
-
-
-#
-## iterate over the three different boundary conditions
-#for bc in boundary_conditions:
-#    
-#    print(f"Calculating for {bc} boundary condition")        
-#        
-#    # constant surface temperature
-#    if bc == "Dirichlet":
-#        
-#        start_dirich_an = time.time()
-#        # store temperature profiles for this boundary condition
-#        T_analytical[bc] = {}
-#        
-#        # constant surface temperature at T_surface = 800 K
-#        T_surface = 800
-#        
-#        # calculate analytical solution
-#        for t in range(1,time_total):
-#                    
-#            # calculate the analytical solution for the temperature profile
-#            T_ansol = T_surface + (T_initial - T_surface)*special.erf(x_grid/(2 * np.sqrt(alpha * t)))
-#            # save temperature data into the analytical temperature
-#            T_analytical[bc][t] = T_ansol
-#        print(f"Time taken for {bc} analytical solution: {np.round(time.time() - start_dirich_an,2)} seconds")
-#        
-#        
-#        start_dirich_num = time.time()
-#        # calculate the numerical solution
-#        bc_data = T_surface
-#        T_num = cn_solver(x_grid, t_grid, upsilon, bc, bc_data, space_divisions, T_initial, dx, k)
-#        # save temperature data into numerical solution
-#        T_numerical[bc] = T_num
-#        print(f"Time taken for {bc} numerical solution: {np.round(time.time() - start_dirich_num,2)} seconds")
-#     
-#    # constant surface heat flux
-#    elif bc == "Neunman":
-#        
-#        start_neunm_an = time.time()
-#        # store temperature profiles for this boundary condition
-#        T_analytical[bc] = {}
-#        T_numerical[bc] = {}
-#        
-#        # constant surface heat flux of q = 5 kW/m2
-#        q = 5000
-#        
-#        # iterate over each time step to calculate and store the analytical and numerical temperatures
-#        for t in range(1,time_total):
-#                    
-#            # calculate the analytical solution for the temperature profile
-#            T_ansol = T_initial + \
-#                    (2*q/k)*np.sqrt(alpha*t/np.pi)*np.exp(-x_grid**2/(4 * alpha * t)) - \
-#                    (q*x_grid/k)*special.erfc(x_grid/(2 * np.sqrt(alpha * t)))
-#            # save temperature data into the analytical temperature
-#            T_analytical[bc][t] = T_ansol
-#        print(f"Time taken for {bc} analytical solution: {np.round(time.time() - start_neunm_an,2)} seconds")
-#
-#        start_neum_num = time.time()
-#        # calculate the numerical solution
-#        bc_data = q
-#        T_num = cn_solver(x_grid, t_grid, upsilon, bc, bc_data, space_divisions, T_initial, dx, k)
-#        # save temperature data into numerical solution
-#        T_numerical[bc] = T_num
-#        print(f"Time taken for {bc} numerical solution: {np.round(time.time() - start_neum_num,2)} seconds")
-#        
-#    # surface convective heating
-#    elif bc == "Robin":
-#        
-#        start_robin_an = time.time()
-#        # store temperature profiles for this boundary condition
-#        T_analytical[bc] = {}
-#        T_numerical[bc] = {}
-#        
-#        # surface convection with a convective heat transfer coefficient of 50 W/m2K and a gas temperature of 800 K
-#        h = 50               # W/m2K
-#        T_infinity = 800     # K
-#        
-#        # iterate over each time step to calculate and store the analytical and numerical temperatures
-#        for t in range(1,time_total):
-#                    
-#            # calculate the analytical solution for the temperature profile
-#            T_ansol = T_initial + \
-#                    (T_infinity - T_initial)*(\
-#                    special.erfc(x_grid/(2*np.sqrt(alpha*t))) - \
-#                    np.exp(h*x_grid/k + h**2*alpha*t/k**2)*special.erfc(x_grid/(2*np.sqrt(alpha*t)) + h*np.sqrt(alpha*t)/k) \
-#                    )
-#
-#            # save temperature data into the analytical temperature
-#            T_analytical[bc][t] = T_ansol    
-#        print(f"Time taken for {bc} analytical solution: {np.round(time.time() - start_robin_an,2)} seconds")
-#        
-#        start_robin_num = time.time()
-#        # calculate the numerical solution
-#        bc_data = h, T_infinity
-#        T_num = cn_solver(x_grid, t_grid, upsilon, bc, bc_data, space_divisions, T_initial, dx, k)
-#        # save temperature data into numerical solution
-#        T_numerical[bc] = T_num
-#        print(f"Time taken for {bc} numerical solution: {np.round(time.time() - start_robin_num,2)} seconds")
-#
-## since analytical solutions are evaluted at 1 Hz, drop columns from numerical solution to facilitate plotting
-#T_numerical_int = {}
-#list_indexes = []
-#
-#for bc in boundary_conditions:
-#    T_numerical_int[bc] = {}
-#    for i in range(1,time_total):
-#        for key in T_numerical[bc].keys():
-#            if float(key) > i:
-#                T_numerical_int[bc][i] = T_numerical[bc][key]
-#                break
-#            continue 
-#    
-## condense all temperatures into one dictionary
-#validation_temperatures = {"analytical": T_analytical, "numerical": T_numerical_int}
-#
-## save in a pickle to retrieve and plot later
-#with open('validation_temperatures.pickle', 'wb') as handle:
-#    pickle.dump(validation_temperatures, handle)
-#    print("Temperature data saved into validation_temperatures.pickle")
-#    
-#grid_time = [x_grid, time_total]
-#with open('grid_time.pickle', 'wb') as handle:
-#    pickle.dump(grid_time, handle)
