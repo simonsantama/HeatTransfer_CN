f864c4d77032cc32cb36510808c76c26697d54ab
SANTAMARIA GARCIA Simon <s1475174@ed.ac.uk>
2020-10-25 20:36:30 +0000

    Changed a couple of things, mainly expanded capabilities do that the CN solver could be used together with the expermental data I gathered from other studies in chapter 3 of the thesis

--- constant_properties/main_constantproperties.py
+++ constant_properties/main_constantproperties.py
@@ -30,8 +30,8 @@ def main_CN_constantproperties(heat_flux, surface_losses, backface_losses, time,
         Type of surface losses to be considered and necessary parameters
     backface_losses: list
         Type of backface losses to be considered and necessary parameters
-    time : int
-        Total testing time
+    time : list
+        List of total testing times
     space : np.array
         Spatial grid and necessary data for implemententing CN with two materials
     properties : list
@@ -72,33 +72,38 @@ def main_CN_constantproperties(heat_flux, surface_losses, backface_losses, time,
     # calculate the temporal grid
     dx = x_grid[1] - x_grid[0]
     dt = (1 / 3) * (dx**2 / alpha)
-    t_grid = np.arange(0,time[0],dt)
+    t_grid = []
+    for t in time:
+        t_grid.append(np.arange(0,t,dt))
     all_data["t_grid"] = t_grid
     upsilon = (alpha*dt)/(2*dx**2)
 
     # filter the shape of the heat flux array according to the type of heat flux used
     hf = []
     if heat_flux[0] == "constant":
-        for i in heat_flux[1]:
-            hf.append(i + np.zeros_like(t_grid))
+        for index, i in enumerate(heat_flux[1]):
+            hf.append(i + np.zeros_like(t_grid[index]))
     elif heat_flux[0] == "linear":
-        for i in heat_flux[1]:
-            hf.append(i*t_grid)
+        for index,i in enumerate(heat_flux[1]):
+            hf.append(i*t_grid[index])
     elif heat_flux[0] == "quadratic":
-        for i in heat_flux[1]:
-            hf.append(i*t_grid**2)
-    elif heat_flux[0] == "cubic":
-        pass
+        for index, i in enumerate(heat_flux[1]):
+            hf.append(i*t_grid[index]**2)
+    elif heat_flux[0] == "parabolic":
+        for index, (a,b) in enumerate(zip(heat_flux[1], heat_flux[2])):
+            hf.append(a*t_grid[index]**2 + b*t_grid[index])
     
     all_data["heat_fluxes"] = hf
     
     # iterate over every heat flux given
     for i, hf_array in enumerate(hf):
         
+        print(f"  calculating hf {i+1} out of {len(hf)}")
+        
         # calculate temperature profiles in time
         calculated_temperatures = solver(heat_flux = hf_array*1000, temp_initial = temperature_initial, 
                                           temp_air = temperature_air, k = k, alpha = alpha,
-                                          x_grid = x_grid, t_grid = t_grid, upsilon = upsilon,
+                                          x_grid = x_grid, t_grid = t_grid[i], upsilon = upsilon,
                                           bc_surface = surface_losses, sigma = 5.67e-8 )
         
         all_data[f"IHF_{heat_flux[0]}_{heat_flux[1][i]}"] = calculated_temperatures
